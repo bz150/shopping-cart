03df99955d6039ed6398dc607bf40b124346a948
Bryan Zhou <bz150@georgetown.edu>
2021-02-22 20:54:39 -0500

    Added email receipt (unfinished)
    Started adding email receipt—code taken from Prof. Rossetti's instructions, not edited. Error with the dotenv

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # shopping-cart
 Shopping cart project for OPIM 243 Python class
+
+[Project Description](https://github.com/prof-rossetti/intro-to-python/blob/master/projects/shopping-cart/README.md)
+
+## Installation
+Clone or download from [GitHub](https://github.com/bz150/shopping-cart)
+
+## Usage
+To run the program, 
--- shopping_cart.py
+++ shopping_cart.py
@@ -101,3 +101,41 @@ print("---------------------------------")
 
 print("THANKS, SEE YOU AGAIN SOON!")
 print("---------------------------------")
+
+
+# 
+# SENDGRID EMAIL RECEIPT
+# 
+import os
+from dotenv import load_dotenv
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+
+load_dotenv()
+
+SENDGRID_API_KEY = os.getenv("SENDGRID_API_KEY", default="OOPS, please set env var called 'SENDGRID_API_KEY'")
+SENDER_ADDRESS = os.getenv("SENDER_ADDRESS", default="OOPS, please set env var called 'SENDER_ADDRESS'")
+
+client = SendGridAPIClient(SENDGRID_API_KEY) #> <class 'sendgrid.sendgrid.SendGridAPIClient>
+print("CLIENT:", type(client))
+
+subject = "Your Receipt from the Green Grocery Store"
+
+html_content = "Hello World"
+print("HTML:", html_content)
+
+# FYI: we'll need to use our verified SENDER_ADDRESS as the `from_email` param
+# ... but we can customize the `to_emails` param to send to other addresses
+message = Mail(from_email=SENDER_ADDRESS, to_emails=SENDER_ADDRESS, subject=subject, html_content=html_content)
+
+try:
+    response = client.send(message)
+
+    print("RESPONSE:", type(response)) #> <class 'python_http_client.client.Response'>
+    print(response.status_code) #> 202 indicates SUCCESS
+    print(response.body)
+    print(response.headers)
+
+except Exception as err:
+    print(type(err))
+    print(err)
